fb68d1dba3f8a72a8a8ee970304aaee370f95443
Najimuddin Shaikh <sherlockshaikh@gmail.com>
2025-02-20 22:08:28 +0530

    backend updated

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,11 +1,76 @@
-export const signup =(req, res) => {
-    res.send("signup route");
-}
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateTokens } from "../lib/utils.js";
 
-export const login =(req, res) => {
-    res.send("login route");
-}
+export const signup = async (req, res) => {
+  // res.send("signup route");
+  console.log(req.body);
+  const { fullName, email, password } = req.body;
 
-export const logout =(req, res) => {
-    res.send("logout route");
-}
+  try {
+    if (!fullName || !password || !email) {
+      return res.status(400).json({ message: "All field are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "Email already exists" });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokens(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (err) {
+    console.log("Error in Signup controller", err.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!isPassword) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    generateTokens(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (err) {
+    console.log("Error in Login Credentials", err.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const logout = (req, res) => {
+  res.send("logout route");
+};
--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 dotenv.config()
 
-dotenv
 export const connectDB = async ()=>{
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
