cac2cb2b23255eea78ee58858adefdcd353fc9f9
Najimuddin Shaikh <116558604+NajimuddinS@users.noreply.github.com>
2025-07-02 18:48:44 +0530

    Update huggingface.js

--- Backend/src/lib/huggingface.js
+++ Backend/src/lib/huggingface.js
@@ -5,7 +5,7 @@ import { config } from "dotenv";
 config();
 
 const HUGGING_FACE_API_URL = "https://api-inference.huggingface.co/models/";
-const MODEL_NAME = "mistralai/Mixtral-8x7B-Instruct-v0.1"; // You can change this to any model you prefer
+const MODEL_NAME = "openai-community/gpt2"; // You can change this to any model you prefer
 
 // Check if the API key exists
 const API_KEY = process.env.HUGGING_FACE_API_KEY;
